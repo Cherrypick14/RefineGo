0f55774a3175c51ef5044409d6751af92554df1f
Cherrypick14 <cherrypicklet@gmail.com>
2024-05-07 21:35:03 +0300

    [feat]: Add capitalize functionality

--- main.go
+++ main.go
@@ -3,57 +3,40 @@ package main
 import (
 	"fmt"
 	"strings"
-	"unicode"
-	// "os"
-	// "strings"
-	//  "reloaded/utils"
+	"os"
+	
 )
 
 func main() {
 	// grab arguments from the command line
-	//    args := os.Args[1:]
+	   args := os.Args[1:]
 
-	// throw error if args is not 3
-	//    if len(args) != 3 {
-	// 	 fmt.Println("Usage: <sample> <result>")
-	//    }
-	// Read file contents
-	//    inputText, err := os.ReadFile(args[0])
+	   inputtext := args[0]
 
-	// throw error reading file
-	//    if err != nil {
-	// 	  fmt.Println("Error reading file")
-	//    }
-	//split text and separate with spaces
-	//    words := strings.Split(string(inputText), " ")
-	word := []string{"it(cap) should happen"}
-	capitalized := Capitalize(word)
-	fmt.Println(capitalized)
-
+	   words, err := os.ReadFile(inputtext)
+	   if err != nil {
+		  fmt.Println("Error reading file")
+		  return
+	   }
+	   content := strings.Split(string(words), " ")
+	   capitalized := CapitalizeAll(content)
+	   fmt.Println(capitalized)
 	// Write to file
 	//    os.WriteFile(args[1], []byte(word), 0644)
 
 }
 
-func Capitalize(words []string) string {
-	for i := 0; i < len(words); i++ {
-		word := words[i]
-		if word == "cap" {
-			prevWord := words[i-1]
-			capitalizeWord := CapitalizeFirstWord(prevWord)
-			words[i-1] = capitalizeWord
-			words = append(words[:i], words[i+1:]...)
+func CapitalizeAll(content []string) string {
+	for i := 0; i < len(content); i++ {
+		if content[i] == "(cap)" && i > 0 {
+			prevWord := content[i-1]
+			if len(prevWord) > 0 {
+				// Capitalize the first letter of the previous word
+				firstChar := strings.ToUpper(string(prevWord[0]))
+				prevWord = firstChar + prevWord[1:]
+				content[i-1] = prevWord
+			}
 		}
 	}
-	return strings.Join(words, "")
-}
-
-func CapitalizeFirstWord(word string) string {
-	if len(word) == 0 {
-		return word
-	}
-	runes := []rune(word)
-	runes[0] = unicode.ToUpper(runes[0])
-
-	return string(runes)
+	return strings.Join(content, " ")
 }
